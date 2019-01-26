6c297b957bb0847bc7e7579d056efc401ad749ee
Matthew Ferderber <matthewferderber@gmail.com>
2019-01-26 18:46:28 -0500

    Use Links for routing

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Firebase from './components/Firebase';
 import Swiper from './components/Swiper/Swiper';
 import WorkspaceCreator from './components/WorkspaceCreator/WorkspaceCreator';
 import WorkspaceList from './components/WorkspaceList/WorkspaceList';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 class App extends Component {
     constructor(props) {
@@ -34,9 +34,9 @@ class App extends Component {
         let links;
         if (this.state.user) {
             links = [
-                (<li key="1"><a href="/#/matching"><h2>Matching</h2></a></li>),
-                (<li key="2"><a href="/#/workspaces"><h2>Workspaces</h2></a></li>),
-                (<li key="3"><a href="/#/createworkspace"><h2>Workspace Creator</h2></a></li>),
+                (<li key="1"><Link to="/matching"><h2>Matching</h2></Link></li>),
+                (<li key="2"><Link to="/workspaces"><h2>Workspaces</h2></Link></li>),
+                (<li key="3"><Link to="/createworkspace"><h2>Workspace Creator</h2></Link></li>),
                 (<li key="4" onClick={this.logOut}><h2>Logout</h2></li>)
             ];
         } else {
@@ -50,10 +50,10 @@ class App extends Component {
                     <ul className="toolbar">
                     {links}
                     </ul>
-                    <Route path="/#/workspaces" component={WorkspaceList}/>
-                    <Route path="/#/createworkspace" component={WorkspaceCreator}/>
-                    <Route path="/#/matching" render={(props) => <Swiper {...props} user={this.state.user}/>} />
-                    <Route path="/#/createprofile" component={WorkspaceCreator}/>
+                    <Route path="/workspaces" component={WorkspaceList}/>
+                    <Route path="/createworkspace" component={WorkspaceCreator}/>
+                    <Route path="/matching" render={(props) => <Swiper {...props} user={this.state.user}/>} />
+                    <Route path="/createprofile" component={WorkspaceCreator}/>
                 </div>
             </Router>
         )
