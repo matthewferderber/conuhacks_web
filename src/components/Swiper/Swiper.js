3f8ca58d3ff0d7c1eb84e0226aba0bf5dd8ae880
nlinnicole <nlin.nicole@gmail.com>
2019-01-26 15:06:31 -0500

    created Swiper component

---
+++ src/components/Swiper/Swiper.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './Swiper.css';
+
+class Swiper extends Component {
+    constructor() {
+
+    }
+    
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
