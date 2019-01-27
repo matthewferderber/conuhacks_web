09ad17818a17885c0f79f820032fa3f3dd66e012
nlinnicole <nlin.nicole@gmail.com>
2019-01-26 22:35:17 -0500

    rounded profile corners

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,30 +4,30 @@ import { connect } from 'react-firebase';
 
 class Profile extends Component{
     constructor(props) {
-    super(props);
+        super(props);
     }
     render() {
         return(
-            <div class="card">
-                <div class="profile">
-                    <div class="profile-img">
-                    {/* //get picture of user */}
-                    </div>
-                    <div class="profile-name">
-                        <h1>Bongo Cat</h1>
-                    </div>
-                    <div class="profile-info">
-                        School: Concordia University
-                        <br />
-                        Year: 2nd Year
-                        <br />
-                        Skills: Java, Javascript, Node.Javascript
-                        <br />
-                        Interests: Web applications, Backend
-                        
+            <div id="card-container">
+                <div class="card">
+                    <div class="profile">
+                        <div class="profile-img">
+                        {/* //get picture of user */}
+                        </div>
+                        <div class="profile-name">
+                            <h1>Bongo Cat</h1>
+                        </div>
+                        <div class="profile-info">
+                            School: Concordia University
+                            <br />
+                            Year: 2nd Year
+                            <br />
+                            Skills: Java, Javascript, Node.Javascript
+                            <br />
+                            Interests: Web applications, Backend
+                        </div>
                     </div>
                 </div>
-
             </div>
         )
     }
